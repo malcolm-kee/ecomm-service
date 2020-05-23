079419e42f1ec1f69803149daef81be78c3ce4d4
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-05-23 13:25:59 +0800

    add heroku setup

--- seed/index.js
+++ seed/index.js
@@ -1,5 +1,41 @@
+const agent = require('superagent');
 const { seedProducts } = require('./seed-products');
+const { baseUrl } = require('./constants');
+
+const tryUntil = (callback, { timeout = 3000, retries = 3 } = {}) =>
+  new Promise((fulfill, reject) => {
+    let retryCount = 0;
+
+    const retry = error => {
+      if (retryCount < retries) {
+        retryCount++;
+        setTimeout(runCode, timeout);
+      } else {
+        reject(error);
+      }
+    };
+
+    const runCode = () => {
+      try {
+        Promise.resolve(callback())
+          .then(fulfill)
+          .catch(retry);
+      } catch (err) {
+        retry(err);
+      }
+    };
+
+    runCode();
+  });
 
 (async function seedData() {
+  await tryUntil(() =>
+    agent.get(`${baseUrl}/api`).then(res => {
+      if (!res.ok) {
+        throw new Error(`Response not ok`);
+      }
+    })
+  );
+
   await seedProducts();
 })();
